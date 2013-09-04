b92c97572d442d8dfa912b51f8c9ff9515532b00
Tristan Le Toullec <tristan.letoullec@gmail.com>
2013-09-04 16:33:46 +0200

    Ajout des type de secret et type de machine
    Ajout du support ATVocabularyManager

--- collective/secretskeeper/secret.py
+++ collective/secretskeeper/secret.py
@@ -18,11 +18,44 @@ from Products.CMFCore.utils import getToolByName
 from plone.autoform.interfaces import IFormFieldProvider
 from zope.interface import alsoProvides
 
+from zope.schema.interfaces import IVocabularyFactory
+
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.component import queryUtility
+
+
+class GetTypeMachineVoc(object):
+    grok.implements(IVocabularyFactory)
+    
+    def __call__(self, context):
+        vtool = getToolByName(context, 'portal_vocabularies')
+        vocab=vtool.getVocabularyByName('typemachine')
+        terms=[]
+        for vocabkey in vocab.getVocabularyDict().iterkeys():
+            terms.append(SimpleVocabulary.createTerm(vocabkey,vocab.getVocabularyDict()[vocabkey]))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(GetTypeMachineVoc, name=u"collective.secretskeeper.typemachinevoc")
+
+class GetTypeSecretVoc(object):
+    grok.implements(IVocabularyFactory)
+    
+    def __call__(self, context):
+        vtool = getToolByName(context, 'portal_vocabularies')
+        vocab=vtool.getVocabularyByName('typesecret')
+        terms=[]
+        for vocabkey in vocab.getVocabularyDict().iterkeys():
+            terms.append(SimpleVocabulary.createTerm(vocabkey,vocab.getVocabularyDict()[vocabkey]))
+        return SimpleVocabulary(terms)
+
+grok.global_utility(GetTypeSecretVoc, name=u"collective.secretskeeper.typesecretvoc")
+
 
 class ISecret(form.Schema):
     """Un secret a garder
     """
+    
     secrettxt = schema.TextLine(
             title=(u"Secret texte"),
             description=(u"Si le secret est sous forme de texte merci de le placer ici."),
@@ -32,6 +65,18 @@ class ISecret(form.Schema):
             title=(u"Secret fichier"),
             description=(u"Si le secret est sous forme de fichier merci de le placer ici."),
             required=False,
+        )
+    typemachine = schema.Choice(
+            title=(u"Type de machine"),
+            description=(u""),
+            required=False,
+            vocabulary='collective.secretskeeper.typemachinevoc'
+        )
+    typesecret = schema.Choice(
+            title=(u"Type de secret"),
+            description=(u""),
+            required=False,
+            vocabulary='collective.secretskeeper.typesecretvoc'
         )
     datestart = schema.Date(
             title=(u"Date de mise en service du secret"),

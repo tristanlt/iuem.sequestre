39eca1aaba679a2fabd79e868e3287e9efc28da3
Tristan Le Toullec <tristan.letoullec@gmail.com>
2013-09-20 17:03:52 +0200

    Gros travail sur les workflows suite a réflexion de CM

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(name='iuem.sequestre',
           'collective.js.jqueryui',
           'collective.z3cform.datetimewidget',
           'collective.js.datatables',
+          'plone.app.versioningbehavior'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
